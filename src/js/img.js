ff126041cf64797102ab97414c2722d67d395926
innabatalova <batalovainna97@gmail.com>
2022-08-25 15:02:41 +0300

    Create: added main fon

--- src/js/img.js
+++ src/js/img.js
@@ -1,15 +1,16 @@
 import Logo from "../../static/images/header-logo-icon.svg";
 import iconPhone from "../../static/images/header-phone-icon.svg";
+import mainFon from "../../static/images/main-fon.png";
 import mainImg from "../../static/images/main-img.jpg";
 
 const headerLogoIcon = document.querySelector(".header__logo__icon");
-const logoTop = Logo;
-headerLogoIcon.style = `background-image:url(${logoTop})`;
+headerLogoIcon.style = `background-image:url(${Logo})`;
 
 const headerPhoneIcon = document.querySelector(".header__phone__icon");
-const iconTop = iconPhone;
-headerPhoneIcon.style = `background-image:url(${iconTop})`;
+headerPhoneIcon.style = `background-image:url(${iconPhone})`;
+
+const mainInfo = document.querySelector(".main");
+mainInfo.style = `background-image:url(${mainFon})`;
 
 const mainImage = document.querySelector(".main__img");
-const mainFon = mainImg;
-mainImage.innerHTML = `<img src=${mainFon} alt='main-img'/>`;
+mainImage.innerHTML = `<img src=${mainImg} alt='main-img'/>`;
