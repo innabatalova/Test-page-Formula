9a93643436027966542a30f3dddf8f4e60567f35
innabatalova <batalovainna97@gmail.com>
2022-08-25 19:48:02 +0300

    Create: added offer style

--- src/js/img.js
+++ src/js/img.js
@@ -2,6 +2,7 @@ import Logo from "../../static/images/header-logo-icon.svg";
 import iconPhone from "../../static/images/header-phone-icon.svg";
 import mainFon from "../../static/images/main-fon.png";
 import mainImg from "../../static/images/main-img.jpg";
+import offer from "../../static/images/offer-img.jpg";
 
 const headerLogoIcon = document.querySelector(".header__logo__icon");
 headerLogoIcon.style = `background-image:url(${Logo})`;
@@ -14,3 +15,6 @@ mainInfo.style = `background-image:url(${mainFon})`;
 
 const mainImage = document.querySelector(".main__img");
 mainImage.innerHTML = `<img src=${mainImg} alt='main-img'/>`;
+
+const offerImage = document.querySelector(".offer");
+offerImage.style = `background-image:url(${offer})`;
