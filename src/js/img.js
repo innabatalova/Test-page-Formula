d36ef6b75865559d0709a8ff234cd1b56cbfc962
innabatalova <batalovainna97@gmail.com>
2022-08-26 12:33:42 +0300

    Create: added footer style

--- src/js/img.js
+++ src/js/img.js
@@ -3,6 +3,9 @@ import iconPhone from "../../static/images/header-phone-icon.svg";
 import mainFon from "../../static/images/main-fon.png";
 import mainImg from "../../static/images/main-img.jpg";
 import offer from "../../static/images/offer-img.jpg";
+import footerMessageIcon from "../../static/images/footer-message-icon.svg";
+import footerPhoneIcon from "../../static/images/footer-phone-icon.svg";
+import footerMailIcon from "../../static/images/footer-mail-icon.svg";
 
 const headerLogoIcon = document.querySelector(".header__logo__icon");
 headerLogoIcon.style = `background-image:url(${Logo})`;
@@ -18,3 +21,12 @@ mainImage.innerHTML = `<img src=${mainImg} alt='main-img'/>`;
 
 const offerImage = document.querySelector(".offer");
 offerImage.style = `background-image:url(${offer})`;
+
+const footerIconMessage = document.querySelector(".footer__icon__message");
+footerIconMessage.style = `background-image:url(${footerMessageIcon})`;
+
+const footerPhoneMessage = document.querySelector(".footer__icon__phone");
+footerPhoneMessage.style = `background-image:url(${footerPhoneIcon})`;
+
+const footerMailMessage = document.querySelector(".footer__icon__mail");
+footerMailMessage.style = `background-image:url(${footerMailIcon})`;
